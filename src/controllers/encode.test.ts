b5c9c9dd0ee49da623d55a0ee2819d8cd6ea8472
Quentin Hello <9997584+qhello@users.noreply.github.com>
2023-10-12 12:44:38 +0200

    fix: validate encode input

--- src/controllers/encode.test.ts
+++ src/controllers/encode.test.ts
@@ -1,23 +1,27 @@
 import request from 'supertest';
 import app from '../app';  // Adjust the path to your actual Koa app file
 
-const handler = request(app.callback()).post('/encode')
+const handler = () => request(app.callback()).post('/encode')
 
 describe('Shorten URL endpoint', () => {
     it('should shorten the URL and return a short URL ID', async () => {
         const testUrl = "http://example.com";
 
-        const response = await handler.send({ url: testUrl });
+        const response = await handler().send({ url: testUrl });
 
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('shortUrl');
         expect(typeof response.body.shortUrl).toBe('string');
+        expect(response.body.shortUrl.length).toBe(12);
+
     });
 
     it('should return a 400 status for invalid URL input', async () => {
         const invalidUrl = "not-a-valid-url";
 
-        const response = await handler.send({ url: invalidUrl });
+        const response = await handler().send({ url: invalidUrl });
+
+        console.log(response.body)
 
         expect(response.status).toBe(400);
     });
@@ -26,11 +30,11 @@ describe('Shorten URL endpoint', () => {
         const testUrl = "http://example.com";
 
         // First request to get the shortId
-        const firstResponse = await handler.send({ url: testUrl });
+        const firstResponse = await handler().send({ url: testUrl });
         const firstShortId = firstResponse.body.shortUrl;
 
         // Second request with the same URL
-        const secondResponse = await handler.send({ url: testUrl });
+        const secondResponse = await handler().send({ url: testUrl });
         const secondShortId = secondResponse.body.shortUrl;
 
         expect(firstShortId).toBe(secondShortId);
--- src/controllers/encode.ts
+++ src/controllers/encode.ts
@@ -6,11 +6,21 @@ import { CACHE } from '../cache'
 export default async (ctx: Context): Promise<void> => {
     const { url } = ctx.request.body
 
+    let parsedUrl: string
+
+    // Verify input
+    try {
+        parsedUrl = new URL(url).toString()
+    } catch (error) {
+        ctx.status = 400
+        return
+    }
+
     // generate a new short id
-    const shortId = shortIdGenerator()
+    const shortId = shortIdGenerator(12)
 
     // save to cache
-    CACHE[shortId] = url
+    CACHE[shortId] = parsedUrl
 
     // return
     ctx.body = { shortUrl: shortId }
